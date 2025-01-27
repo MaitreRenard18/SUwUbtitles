538b3f0e475e368467bdc71997635d6adfe578e4
MaitreRenard18 <maitre.renardowo@gmail.com>
2025-01-27 13:06:10 +0100

    Working V1

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.responses import FileResponse
 
 from subtitles import generate_subtitles
 from video import add_subtitles
@@ -21,13 +22,6 @@ async def root(request: Request):
         request=request, name="upload.html", context={}
     )
 
-
-async def streamer(file: _TemporaryFileWrapper):
-    file.seek(0)
-    while chunk := file.read(1024 * 1024):
-        yield chunk
-
-
 @app.post("/upload/")
 async def upload(video: UploadFile):
     with TemporaryFile(mode="+bw", suffix=".mp4", delete=False) as base_video:
@@ -35,8 +29,7 @@ async def upload(video: UploadFile):
         
         subtitles = generate_subtitles(base_video.name)
         
-        final_video = TemporaryFile("+bw", delete=False)
-        add_subtitles(base_video.name, subtitles, final_video.name)
-        os.remove(subtitles)
+        final_video = TemporaryFile("+bw", delete=False, suffix=".mp4")
+        add_subtitles(base_video.name, subtitles.name, final_video.name)
         
-        return StreamingResponse(streamer(final_video))
+        return FileResponse(final_video.name, media_type='application/octet-stream',filename=video.filename)
